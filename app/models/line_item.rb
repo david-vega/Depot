12f8068846e8785c32c36afda99b812f56c70ff5
David Vega <david.vega@crowdint.com>
2012-08-06 18:31:38 -0500

    finished shoping cart with testing

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,17 @@
+# == Schema Information
+#
+# Table name: line_items
+#
+#  id         :integer          not null, primary key
+#  product_id :integer
+#  cart_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
   belongs_to :product
   belongs_to :cart
+  accepts_nested_attributes_for :product
+  attr_accessible :cart_id, :product_id, :product_attributes, :product
 end
